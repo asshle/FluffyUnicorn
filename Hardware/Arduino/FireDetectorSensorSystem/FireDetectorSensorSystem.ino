365e33257a4b08fc943d64f1156fde2f94dcbd1a
asshole3214 <limpinpin123@gmail.com>
2020-06-13 13:46:57 +0800

    Update the esp32 code
    -added in esp32 step file
    -fixed constant updating

--- Hardware/Arduino/FireDetectorSensorSystem/FireDetectorSensorSystem.ino
+++ Hardware/Arduino/FireDetectorSensorSystem/FireDetectorSensorSystem.ino
@@ -29,15 +29,26 @@ struct timeScheduling
   bool threadHalt = false;
   //TODO:Implement priority que system to allow for immediate soft interrupt
 };
-struct ConnectionData
+struct connectionData
 {
   const char* networkSSID = "";
   const char* networkPassword = "";
-  const String serverLink ="https://fluffyunicorn.mybluemix.net/";
+  const String serverLink = "https://fluffyunicorn.mybluemix.net/";
+  const String MacAddress = WiFi.macAddress();
+};
+struct systemHealth
+{
+  int batteryPercentage;
+  int fireSensorReading;
+  const int fireSensorPin = 36;
+  const int batteryStatusPin = 39 ;
+  long latituide , longituide;
+
 };
 
 timeScheduling multiTasking;
-ConnectionData wifiData;
+connectionData wifiData;
+systemHealth systemHealth;
 
 void setup()
 {
@@ -56,18 +67,24 @@ void loop()
     if ( (unsigned long)(currentTime - multiTasking.sensorReadLastTime) >= multiTasking.sensorReadTimeSlice)
     {
       //readingSensor thread
-      readFireSensor();
+      readSensorsVal();
+      multiTasking.sensorReadLastTime = currentTime;
     }
     if ( (unsigned long)(currentTime - multiTasking.sensorMeshCommLastTime) >= multiTasking.sensorMeshCommTimeSlice)
     {
       //communication with neighbouring sensors here
       sensorMeshReadComm();
+      multiTasking.sensorMeshCommLastTime = currentTime;
     }
-     if ( (unsigned long)(currentTime - multiTasking.cloudCommsLastTime) >= multiTasking.cloudCommsTimeSlice)
+    if ( (unsigned long)(currentTime - multiTasking.cloudCommsLastTime) >= multiTasking.cloudCommsTimeSlice)
     {
-      //cloud comms thread  
-      //TODO: Check why time wait is not working 
-      cloudCommunication("StatusUpdate","?sensorValue=262");
+      //cloud comms thread
+      //will send an update to cloud server every 1 min
+      String parameters = "?sensorValue=262&";
+      parameters += wifiData.MacAddress; 
+      cloudCommunication("StatusUpdate",parameters , currentTime);
+      multiTasking.cloudCommsLastTime = currentTime;
+
     }
   }
   else
@@ -89,6 +106,7 @@ void initialise()
   Serial.begin(9600); // initialise 9600 for now , if bluetooth is used , then switch to 115200
   wifiInitialise();
   //TODO: initialise incoming pin for "simulated" sensor
+  //TODO: Get lat long data here
 
 }
 void wifiInitialise()
@@ -121,14 +139,41 @@ void wifiInitialise()
 }
 
 //========================Hardware/Board Methods=========================
+
+
+/*
+
+   struct systemHealth
+  {
+     int batteryPercentage;
+     int fireSensorReading;
+     long latituide , longituide;
+     int fireSensorPin = A0;
+     int batteryStatusPin = A1;
+  }
+
+*/
 void getLocationData()
 {
   //get current location data
+
 }
-void readFireSensor()
+void readSensorsVal()
 {
   //read fire sensor value here
+
+  //======Actual System Implementation=========================
+  //systemHealth.fireSensorReading = analogRead(systemHealth.fireSensorPin);
+  //systemHealth.batteryPercentage = analogRead(systemHealth.batteryStatusPin);
+
+  //======Prototype/Testing of sensor readings=================
+  systemHealth.fireSensorReading = random(0, 4095); // since actual reading of analog values will be from 0 - 4095
+  systemHealth.batteryPercentage = random(0, 1023); // maximum reading of battery
+
   //for purpose of the prototype , since we do not have any actual fire detector , we will generate random values
+  systemHealth.fireSensorReading = map( systemHealth.fireSensorReading , 0, 4095, 0, 100);
+  systemHealth.batteryPercentage = systemHealth.batteryPercentage / 204.6 ;//for a 5V battery source , 1023/5 =204.6
+
 }
 
 //========================Communications Methods=========================
@@ -140,7 +185,7 @@ void commandEvent()
   Serial.println(command);
   if (command == "test" )
   {
-    cloudCommunication("StatusUpdate","?sensorValue=262");
+    cloudCommunication("test", "?testData = test", millis());
   }
   else
   {
@@ -148,7 +193,7 @@ void commandEvent()
   }
 
 }
-void cloudCommunication(String apiLink , String parameters)
+void cloudCommunication(String apiLink , String parameters , unsigned long timeStamp)
 {
   //establish communications with Node red command center
   /*
@@ -161,9 +206,9 @@ void cloudCommunication(String apiLink , String parameters)
      msg.payload.Temperature = getTemp();
   */
   String getReq = wifiData.serverLink;
-  getReq+=apiLink;
-  getReq+=parameters;
-  Serial.println(getReq);
+  getReq += apiLink;
+  getReq += parameters;
+  Serial.print(getReq); Serial.print(" millis at : "); Serial.println(timeStamp);
   if (WiFi.status() == WL_CONNECTED)
   {
     //Check the current connection status
